f78b1317ec601d4cb1642ceb45a60839c6620f2e
Yafet Tesfaye <yafetesfaye11@gmail.com>
2024-08-10 04:49:04 -0700

    Initialized

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+
+
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+public class ApplicationUser : IdentityUser {
+    [Required]
+    public required string FirstName { get; set;}
+    [Required]
+    public required string LastName { get; set;}
+    public DateTime CreatedTime { get; set;}
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProjectManagementApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
