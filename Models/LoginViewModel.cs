da183f4d2ae2f782a64505e41b2354530df862b8
Yafet Tesfaye <yafetesfaye11@gmail.com>
2024-08-10 05:53:02 -0700

    Add LoginView To Model

---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,12 @@
+
+using System.ComponentModel.DataAnnotations;
+
+public class LoginViewModel {
+    [Required]
+    public  string UserName {get;set;} = "";
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password {get;set;} = "";  
+    
+}
