e8a875b306dfbcf28afcb5480a8329aeb73c6990
Yafet Tesfaye <yafetesfaye11@gmail.com>
2024-08-10 05:14:47 -0700

    Add SuperAdmin Controller to The Controller

---
+++ Controllers/SuperAdminController.cs
@@ -0,0 +1,50 @@
+// to add the admin info
+
+using System.Net;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+[Authorize(Roles ="Super")]
+public class SuperAdminController : Controller {
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly RoleManager<ApplicationRole> _roleManager;
+    private readonly ApplicationDbContext _dbContext;
+    public SuperAdminController(
+        UserManager<ApplicationUser> _userManager,
+        RoleManager<ApplicationRole> _roleManager,
+        ApplicationDbContext _dbContext){
+            this._userManager = _userManager;
+            this._roleManager = _roleManager;
+            this._dbContext = _dbContext;
+        }
+    [HttpGet]
+    [Authorize(Roles ="Super")]
+    public IActionResult Register(){
+        return View();
+    }
+    [Authorize(Roles ="Super")]
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Register(RegisterViewModel model){
+        if (ModelState.IsValid){
+            var user = new ApplicationUser(){
+                FirstName = model.FirstName,    
+                LastName = model.LastName,
+                Email = model.Email,
+                UserName = model.UserName,
+            };
+            var result = await _userManager.CreateAsync(user,model.Password);
+            if (result.Succeeded){
+                // add the current user to admin role
+                var res = await _userManager.AddToRoleAsync(user,"Admin");
+                if (res.Succeeded){
+                    return RedirectToAction("Index","Home");
+                }
+                return View(model);
+            }
+        }
+        return View(model);
+
+    }
+}
