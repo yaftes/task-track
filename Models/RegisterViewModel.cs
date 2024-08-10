40acff0c21fcd53974c3d938e4047d7c134065fc
Yafet Tesfaye <yafetesfaye11@gmail.com>
2024-08-10 05:13:00 -0700

    Add The RegisterViewModel to Model Directory

---
+++ Models/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+
+using System.ComponentModel.DataAnnotations;
+
+public class RegisterViewModel {
+    [Required]
+    public string FirstName { get; set;} = "";
+    [Required]
+    public  string UserName {get;set;} = "";
+    [Required]
+    public string LastName { get; set;} = "";
+    [Required]
+    [EmailAddress]
+    public  string Email { get; set;} = "";
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set;} = "";
+    [Required]
+    [Compare("Password")]
+    public  string ConfirmPassword {get;set;} = "";
+    public List<Skill> Skills { get; set;} = [];
+}
